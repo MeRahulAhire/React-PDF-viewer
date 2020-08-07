9976c9b6fb0b1e0f78ad9863435248e6892595c4
Rahul Ahire <ahirerahul21@gmail.com>
2020-08-07 15:33:37 +0530

    preparing for deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+export class App extends Component {
+	state = {
+		file: ''
+	};
+	onChangeHandler = (e) => {
+		this.setState({ file: e.target.value });
+	};
+	render() {
+		return (
+			<div className="main">
+				<div className="container">
+          <div className="h1">
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <h1>View your PDF File</h1>
+          </div>
+					<input type="file" className="input" name="input" id="input" onChange={this.onChangeHandler} />
+					<div className="label">
+          <label htmlFor="input">
+						<span class="material-icons">add_circle</span>
+					</label>
+          </div>
+					<div className="window">
+          <iframe src={this.state.file} title='This is an PDF window' frameborder="0" width="95%" height="500px" />
+          </div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
